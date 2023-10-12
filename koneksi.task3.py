edfb16c10fc9a09caab5c22afe8960573fc16296
Rafli Hada Setiawan <raflijr47@gmail.com>
2023-10-12 08:02:48 +0700

    Revision

--- koneksi.task3.py
+++ koneksi.task3.py
@@ -2,10 +2,8 @@ import mysql.connector
 
 # Melakukan percobaan koneksi
 conn = mysql.connector.connect(
-    host="localhost",
-    port='3300',
-    user="root",
-    password="something",
+    host="localhost",port='3300',
+    user="root",password="something",
     database="kalbe"
 )
 
@@ -17,22 +15,18 @@ insert_sql = """
 INSERT INTO karyawan (FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
 VALUES (%s, %s, %s, %s, %s)
 """
-
 values = ("John", "Doe", 30, "Male", 50000)
 
 try:
     # Eksekusi sql command
     cursor.execute(insert_sql, values)
-
     # Melakukan perubahan (commit) pada db
     conn.commit()
     print("Data inserted successfully!")
-
 except Exception as e:
     # Roll back apabila ada issue
     conn.rollback()
     print(f"Error: {e}")
-
 finally:
     # Menutup koneksi
     cursor.close()
